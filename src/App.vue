42de4a26369bdcf4221cbae8bf48155fdd5aa642
tmc <1018663766@qq.com>
2019-04-19 11:07:24 +0800

    add mapState

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,46 @@
     {{this.$store.getters.count}}
     <button @click='change'>点击改变State</button>
     <button @click='asyncChange'>点击异步改变State</button>
+    <ul>
+      <li v-for="(item, index) in arr" :key='index'>
+        <h1>{{item.name}}</h1>
+        <h2>{{item.age}}</h2>
+      </li>
+    </ul>
+    <table border>
+      <tr>
+        <td>姓名</td>
+        <td>年龄</td>
+      </tr>
+      <tr v-for="(item, index) in arr" :key="index">
+        <td>{{item.name}}</td>
+        <td>{{item.age}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 export default {
+  created () {
+    // console.log(this.list)
+  },
+  computed: {
+    // 1.如果获取的数据和state名称相同可以直接使用数组的方式
+    // ...mapState([
+    //   'list'
+    // ])
+    // 2. 如果获取的数据不想和state中的名称相同的话就用对象的形式
+    ...mapState({
+      arr: 'list'
+    })
+  },
+  // 3. 第三种方式使用mapState
+  // computed:mapState({
+  //   // arr: state => state.list
+  //   arr: 'list'
+  // }),
   methods: {
     change () {
       this.$store.commit('ADD_COUNT', 10)
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,9 @@
 const state = {
-  count: 0
+  count: 0,
+  list: [
+    { id: 1, name: 'tmc', age: 24 },
+    { id: 2, name: 'sfm', age: 21 }
+  ]
 }
 
 export default state
