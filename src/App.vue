6b7bb2f86b368d38db86f0409f11b2372d948911
tmc <1018663766@qq.com>
2019-04-19 11:15:15 +0800

    add mapGetters

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,13 @@
         <td>{{item.age}}</td>
       </tr>
     </table>
+    <!-- <p>mapGetters辅助函数对象的方式getters获取list数据{{this.getters_list}}</p> -->
+    <p>mapGetters辅助函数数组的方式getters获取list数据{{this.list}}</p>
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import {mapState, mapGetters} from 'vuex'
 export default {
   created () {
     // console.log(this.list)
@@ -38,12 +40,24 @@ export default {
     // 2. 如果获取的数据不想和state中的名称相同的话就用对象的形式
     ...mapState({
       arr: 'list'
-    })
+    }),
+    // 1. 第一种获取getters
+    // ...mapGetters({
+    //   getters_list: 'list'
+    // })
+    // 2. 第二种获取getters
+    ...mapGetters([
+      'list'
+    ])
   },
   // 3. 第三种方式使用mapState
   // computed:mapState({
   //   // arr: state => state.list
   //   arr: 'list'
+  // }),
+  // 3. 第三种方式使用mapGetters
+  // computed:mapGetters({
+  //   list: 'list'
   // }),
   methods: {
     change () {
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,3 @@
 export const count = state => state.count + 1
+
+export const list = state => state.list
